ba11a0484e74527d4a2d0daf440e4c32f7599fe0
onair-lena <pechenko@bk.ru>
2023-07-26 18:57:00 +0300

    registration method added

--- backend/controllers/useerController.js
+++ backend/controllers/useerController.js
@@ -1,7 +1,26 @@
+const { where } = require('sequelize');
 const ApiError = require('../errors/ApiError');
+const bcrypt = require('bcrypt');
+const { User, Record } = require('../models');
+const jwt = require('jsonwebtoken');
 
 class UserController {
-  async registration(req, res) {}
+  async registration(req, res, next) {
+    const { email, password } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest('Email or password cannot be empty'));
+    }
+    const candidate = await User.findOne({ where: { email } });
+    if (candidate) {
+      return next(ApiError.badRequest('User with this email already exist'));
+    }
+    const hashPAssword = await bcrypt.hash(password, 5);
+    const user = await User.create({ email, password: hashPAssword });
+    const token = jwt.sign({ id: user.id, email }, process.env.SECRET_KEY, {
+      expiresIn: '24h',
+    });
+    return res.json({ token });
+  }
 
   async login(req, res) {}
 
