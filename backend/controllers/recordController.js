d41d3dcd6a036ad1db9641ce9ce140c3872a0743
onair-lena <pechenko@bk.ru>
2023-07-27 17:21:21 +0300

    userId added to getAll method

--- backend/controllers/recordController.js
+++ backend/controllers/recordController.js
@@ -26,11 +26,11 @@ class RecordController {
 
   async getAll(req, res) {
     let { limit, page } = req.query;
-
+    const { id: userId } = req.user;
     page = page || 1;
     limit = limit || 9;
     let offset = page * limit - limit;
-    const records = await Record.findAll({ limit, offset });
+    const records = await Record.findAll({ where: { userId }, limit, offset });
     return res.json(records);
   }
 
