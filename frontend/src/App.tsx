9310d5b74f22a34f6448883fd3591bd310447af6
onair-lena <pechenko@bk.ru>
2023-07-28 21:59:12 +0300

    css modules config added

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,30 +1,14 @@
 import { Route, Routes } from 'react-router-dom';
 import React from 'react';
 import './App.css';
+import { Login } from './pages/login/login';
 
 function App() {
   return (
     <Routes>
-      <Route path="/login" element={<h2>Login</h2>}></Route>
+      <Route path="/login" element={<Login />}></Route>
       <Route path="/" element={<h2>Home</h2>}></Route>
     </Routes>
-
-    // <div className="App">
-    //   <header className="App-header">
-    //     <img src={logo} className="App-logo" alt="logo" />
-    //     <p>
-    //       Edit <code>src/App.js</code> and save to reload.
-    //     </p>
-    //     <a
-    //       className="App-link"
-    //       href="https://reactjs.org"
-    //       target="_blank"
-    //       rel="noopener noreferrer"
-    //     >
-    //       Learn React
-    //     </a>
-    //   </header>
-    // </div>
   );
 }
 
---
+++ frontend/src/pages/login/login.tsx
@@ -0,0 +1,5 @@
+import styles from './styles.module.css';
+
+export const Login = () => {
+  return <div className={styles.wrapper}>Login</div>;
+};
