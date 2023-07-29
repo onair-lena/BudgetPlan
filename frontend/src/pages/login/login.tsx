b034d69ef4b172d7a8e12146fce30fbca69cc293
onair-lena <pechenko@bk.ru>
2023-07-29 21:20:33 +0300

    added background to login page

--- frontend/src/pages/login/login.tsx
+++ frontend/src/pages/login/login.tsx
@@ -1,5 +1,16 @@
 import styles from './styles.module.css';
 
 export const Login = () => {
-  return <div className={styles.wrapper}>Login</div>;
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.loginForm}>Login form</div>
+      <div className={styles.containerImage}>
+        <img
+          className={styles.loginImage}
+          src="/login.jpg"
+          alt="login page image"
+        />
+      </div>
+    </div>
+  );
 };
